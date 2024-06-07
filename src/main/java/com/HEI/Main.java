7f855fde54b794fb70dc24ef2b6d7a41a2ad5ac0
Marc Herilala <hei.marc.3@gmail.com>
2024-06-07 16:56:43 +0300

    feat:add test for each affirmation

--- src/main/java/com/HEI/Main.java
+++ src/main/java/com/HEI/Main.java
@@ -3,18 +3,26 @@ package com.HEI;
 import com.HEI.affirmation.Affirmation;
 import com.HEI.affirmation.Mensonge;
 import com.HEI.affirmation.Verite;
-import com.HEI.composition.CompositionDeDeuxAffirmations;
+import com.HEI.composition.Compositioins;
 import com.HEI.conjonction.ConjonctionDeCoordination;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Verite verite1=new Verite("Lou est beau");
-        Mensonge mensonge1=new Mensonge("Lou est pauvre");
-        Affirmation affirmation1=new Affirmation("Lou est genereux");
-        CompositionDeDeuxAffirmations compositionDeDeuxAffirmations=new
-                CompositionDeDeuxAffirmations(mensonge1,
-                ConjonctionDeCoordination.et,affirmation1);
-        System.out.println(compositionDeDeuxAffirmations.vraiOuFaux());
+        List<Affirmation> affirmations = new ArrayList<>();
+        affirmations.add(new Verite("Lou est beau"));
+        affirmations.add(new Affirmation("Lou est généreux"));
+        affirmations.add(new Mensonge("Lou est pauvre"));
+
+        List<ConjonctionDeCoordination> conjonctionsDeCoordination = new ArrayList<>();
+        conjonctionsDeCoordination.add(ConjonctionDeCoordination.ou);
+        conjonctionsDeCoordination.add(ConjonctionDeCoordination.donc);
+
+        Compositioins composition = new Compositioins(affirmations, conjonctionsDeCoordination);
+        String resultat = composition.vraiOuFaux();
+        System.out.println("Résultat : " + resultat);
     }
 }
--- src/test/java/AffirmationsTest.java
+++ src/test/java/AffirmationsTest.java
@@ -1,7 +1,62 @@
+import com.HEI.affirmation.Affirmation;
+import com.HEI.affirmation.Mensonge;
+import com.HEI.affirmation.Verite;
+import com.HEI.composition.Compositioins;
+import com.HEI.conjonction.ConjonctionDeCoordination;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AffirmationsTest {
     @org.junit.jupiter.api.Test
-    public void hello(){
-        assertEquals(0,0);
+    public void LouEstBeauOuLouEstGenereuxDoncLouEstPauvre(){
+        List<Affirmation> affirmations = new ArrayList<>();
+        affirmations.add(new Verite("Lou est beau"));
+        affirmations.add(new Affirmation("Lou est généreux"));
+        affirmations.add(new Mensonge("Lou est pauvre"));
+
+        List<ConjonctionDeCoordination> conjonctionsDeCoordination = new ArrayList<>();
+        conjonctionsDeCoordination.add(ConjonctionDeCoordination.ou);
+        conjonctionsDeCoordination.add(ConjonctionDeCoordination.donc);
+
+        Compositioins composition = new Compositioins(affirmations, conjonctionsDeCoordination);
+        String resultat = composition.vraiOuFaux();
+        assertEquals("faux",resultat);
+    }
+    @Test
+    public void LouEstPauvreOuLouEstGenereux(){
+        List<Affirmation> affirmations=new ArrayList<>();
+        affirmations.add(new Mensonge("Lou est pauvre"));
+        affirmations.add(new Affirmation("Lou est genereux"));
+        List<ConjonctionDeCoordination> conjonctionDeCoordinations=new ArrayList<>();
+        conjonctionDeCoordinations.add(ConjonctionDeCoordination.ou);
+        Compositioins composition = new Compositioins(affirmations,conjonctionDeCoordinations);
+        String resultat=composition.vraiOuFaux();
+        assertEquals("faux",resultat);
+    }
+    @Test
+    public void LouEstPauvreDoncLouEstGenereux(){
+        List<Affirmation> affirmations=new ArrayList<>();
+        affirmations.add(new Mensonge("Lou est pauvre"));
+        affirmations.add(new Affirmation("Lou est genereux"));
+        List<ConjonctionDeCoordination> conjonctionDeCoordinations=new ArrayList<>();
+        conjonctionDeCoordinations.add(ConjonctionDeCoordination.donc);
+        Compositioins composition = new Compositioins(affirmations,conjonctionDeCoordinations);
+        String resultat=composition.vraiOuFaux();
+        assertEquals("vrai",resultat);
+    }
+    @Test
+
+    public void LouEstPauvreEtLouEstGenereux(){
+        List<Affirmation> affirmations=new ArrayList<>();
+        affirmations.add(new Mensonge("Lou est pauvre"));
+        affirmations.add(new Affirmation("Lou est genereux"));
+        List<ConjonctionDeCoordination> conjonctionDeCoordinations=new ArrayList<>();
+        conjonctionDeCoordinations.add(ConjonctionDeCoordination.et);
+        Compositioins composition = new Compositioins(affirmations,conjonctionDeCoordinations);
+        String resultat=composition.vraiOuFaux();
+        assertEquals("faux",resultat);
     }
 }
