4ffcc5e989448c289659d5b47d011f301ce86b66
Marc985 <hei.marc.3@gmail.com>
2024-09-01 13:01:19 +0300

    chore:add example

--- src/main/java/com/HEI/Main.java
+++ src/main/java/com/HEI/Main.java
@@ -9,10 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Verité verité=new Verité("lou est beau");
-        Incertitude affirmation=new Incertitude("Lou est genereux");
-        Mensonge mensonge=new Mensonge("lou est pauvre");
-     ValeurDeVerité valeurDeVerité1=  verité.ou(affirmation).donc(mensonge).valeurDeVerité();
+       	Verité verite = new Verité("Le ciel est bleu par temps clair.");
+	Incertitude affirmation = new Incertitude("Certaines personnes préfèrent le ciel bleu pour les activités de plein air.");
+	Mensonge mensonge = new Mensonge("Le ciel est toujours bleu, même lorsqu'il pleut.");
+	
+	ValeurDeVerité valeurDeVerité1 = verite.ou(affirmation).donc(mensonge).valeur();
+    	 ValeurDeVerité valeurDeVerité1=  verité.ou(affirmation).donc(mensonge).valeurDeVerité();
         System.out.println(valeurDeVerité1);
 
         System.out.println(mensonge.ou(affirmation).valeurDeVerité());
